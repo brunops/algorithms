bfb34fe257c841446170e1e4233cf2e6d67778ad
Bruno Sanches <brunopsanches@gmail.com>
2013-11-17 18:48:33 -0800

    merge_arys takes two sorted arys and merge them into one sorted array

--- sort/mergesort.rb
+++ sort/mergesort.rb
@@ -1,5 +1,25 @@
 def mergesort(ary)
-  [1, 3, 5]
+  return ary if ary.length == 1
+end
+
+def merge_arys(ary1, ary2)
+  merged_arys = []
+
+  while ary1.length > 0 || ary2.length > 0
+    if ary1[0] && ary2[0]
+      if ary1[0] <= ary2[0]
+        merged_arys << ary1.shift
+      else
+        merged_arys << ary2.shift
+      end
+    elsif ary1[0]
+      merged_arys << ary1.shift
+    else
+      merged_arys << ary2.shift
+    end
+  end
+  
+  merged_arys
 end
 
 def assert(obj)
@@ -7,6 +27,9 @@ def assert(obj)
 end
 
 if $0 === __FILE__
-  assert(mergesort([3, 5, 1]) === [1, 3, 5])
+  assert(merge_arys([1, 7, 19], [5, 18, 22, 30]) == [1, 5, 7, 18, 19, 22, 30])
+#  assert(mergesort([3, 5, 1]) === [1, 3, 5])
+
+  puts "yeah!"
 end
 
