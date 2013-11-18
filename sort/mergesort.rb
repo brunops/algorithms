9ac6f05c112d1bb7d717f05f66997d4d8a40a920
Bruno Sanches <brunopsanches@gmail.com>
2013-11-17 19:01:22 -0800

    Implement Mergesort algorithm
    * Add two tests
    * Return only element if array.size is 1 (base case)
    * Divide and conquer! Divide array into left and right and apply mergesort to each side
    * Force parameters to be arrays by putting the return of mergesort into
    	an array and flattening it

--- sort/mergesort.rb
+++ sort/mergesort.rb
@@ -1,5 +1,10 @@
 def mergesort(ary)
-  return ary if ary.length == 1
+  return ary[0] if ary.length == 1
+
+  left = [mergesort(ary[0...(ary.length / 2)])].flatten
+  right = [mergesort(ary[(ary.length / 2)..-1])].flatten
+
+  merge_arys left, right
 end
 
 def merge_arys(ary1, ary2)
@@ -28,7 +33,8 @@ end
 
 if $0 === __FILE__
   assert(merge_arys([1, 7, 19], [5, 18, 22, 30]) == [1, 5, 7, 18, 19, 22, 30])
-#  assert(mergesort([3, 5, 1]) === [1, 3, 5])
+  assert(mergesort([3, 5, 1]) === [1, 3, 5])
+  assert(mergesort([23,42,34,234,5,5,6,8,65,7,34,45,4,7,8,8,453,0]) == [0, 4, 5, 5, 6, 7, 7, 8, 8, 8, 23, 34, 34, 42, 45, 65, 234, 453])
 
   puts "yeah!"
 end
