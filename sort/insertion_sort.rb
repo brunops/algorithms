ce7a0b5335c7997a82262832aa05dbbabe095a80
Bruno Sanches <brunopsanches@gmail.com>
2013-11-19 13:01:23 -0800

    Implement Insertion sort algorithm
    * Add new test

--- sort/insertion_sort.rb
+++ sort/insertion_sort.rb
@@ -1,5 +1,17 @@
 def insertion_sort array
-  [1, 3, 8]
+  for i in 1...(array.length)
+    key = array[i]
+    j = i - 1
+
+    while j >= 0 && array[j] > key
+      array[j + 1] = array[j]
+      j -= 1
+    end
+
+    array[j + 1] = key
+  end
+
+  array
 end
 
 def assert obj
@@ -8,4 +20,7 @@ end
 
 if $0 == __FILE__
   assert(insertion_sort([8, 3, 1]) == [1, 3, 8])
+  assert(insertion_sort([8, 3, 1, 10, 55, 123, 3, -1]) == [-1, 1, 3, 3, 8, 10, 55, 123])
+
+  puts "yeah!"
 end
