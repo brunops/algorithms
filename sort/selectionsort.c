fc9b3333118c6983104e52ffecb6883fa6f5ebab
Bruno Sanches <brunopsanches@gmail.com>
2013-11-21 13:03:31 -0800

    Implement SelectionSort
    * Rename function name to selectionsort
    * Define min_index variable to store the index of the minimum value in
    the array on each iteration
    * Swap values only once (after second level loop is finished)

--- sort/selectionsort.c
+++ sort/selectionsort.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void bubblesort(int *p, size_t size);
+void selectionsort(int *p, size_t size);
 void print_array(int *p, size_t size);
 
 int main() {
   int a[13] = { 3, 2, 12, 4, 22, 32, 44, 444, 12, 65, 9, -2, -13 };
 
   print_array(a, 13);
-  bubblesort(a, 13);
+  selectionsort(a, 13);
   print_array(a, 13);
 
   return 0;
@@ -21,16 +21,19 @@ void print_array(int *p, size_t size) {
   }
 }
 
-void bubblesort(int *p, size_t size) {
-  int i, j, temp;
+void selectionsort(int *p, size_t size) {
+  int i, j, temp, min_index;
 
   for (i = 0; i < size; i++) {
+    min_index = i;
     for (j = i + 1; j < size; ++j) {
       if (p[j] < p[i]) {
-        temp = p[i];
-        p[i] = p[j];
-        p[j] = temp;
+        min_index = j;   
       }
     }
+
+    temp = p[i];
+    p[i] = p[min_index];
+    p[min_index] = temp;
   }
 }
