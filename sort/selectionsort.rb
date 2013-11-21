aff5f599b28c989f80bc4749587165ff65ed9fde
Bruno Sanches <brunopsanches@gmail.com>
2013-11-21 12:58:02 -0800

    Rename bubblesort to selectionsort
    * Add a min_number_index variable so only one swap is performed on each
    iteration through the array

--- sort/selectionsort.rb
+++ sort/selectionsort.rb
@@ -1,10 +1,12 @@
-def bubblesort ary
-  ary.each_with_index do |el, i|
+def selectionsort ary
+  ary.each_with_index do |el, i|    
+    min_number_index = i
     for j in ((i + 1)...(ary.length))
-      if ary[i] > ary[j]
-        ary[i], ary[j] = ary[j], ary[i]
+      if ary[j] < ary[min_number_index]
+        min_number_index = j
       end
     end
+    ary[i], ary[min_number_index] = ary[min_number_index], ary[i]
   end
 end
 
@@ -13,8 +15,8 @@ def assert obj
 end
 
 if $0 == __FILE__
-  assert(bubblesort([4, -9, 5]) == [-9, 4, 5])
-  assert(bubblesort([4, -9, 123, 33, 321, 9, 0, -90, 111]) == [-90, -9, 0, 4, 9, 33, 111, 123, 321])
+  assert(selectionsort([4, -9, 5]) == [-9, 4, 5])
+  assert(selectionsort([4, -9, 123, 33, 321, 9, 0, -90, 111]) == [-90, -9, 0, 4, 9, 33, 111, 123, 321])
 
   p "oh yeah!"
 end
