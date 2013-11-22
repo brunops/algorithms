c0a5da0ee8f6bc0dcc4e40645bb7ba62cb9eed06
Bruno Sanches <brunopsanches@gmail.com>
2013-11-22 02:05:18 -0800

    Create strassen_multiplication.h header file and move function declarations there

--- matrix/strassen_multiplication.c
+++ matrix/strassen_multiplication.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define BITS_IN_A_BYTE 8
-
-void assert_equals(int a, int b);
-
-void print_bits(int n);
-void print_matrix(int **p, int size);
-void allocate_matrix(int ***p, int size);
-
-int next_power_of_2(int number);
+#include "strassen_multiplication.h"
 
 int main(int argc, char *argv[]) {
   int i, **matrix;
