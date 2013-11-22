7103a19db2bb0cb34a2d73b8f2f44f8769a409f8
Bruno Sanches <brunopsanches@gmail.com>
2013-11-22 15:48:45 -0800

    Reorganize header file grouping functions by its purpose

--- matrix/strassen_multiplication.h
+++ matrix/strassen_multiplication.h
@@ -4,16 +4,20 @@
 
 #define BITS_IN_A_BYTE 8
 
+// Test functions
 void assert_equals(int a, int b);
 void assert_matrix_equals(int **m1, int **m2, int size);
 
+// Debugging
 void print_bits(int n);
 void print_matrix(int **p, int size);
+
+
+// Utility functions
+int next_power_of_2(int number);
 void allocate_matrix(int ***p, int size);
 
-int next_power_of_2(int number);
-
-// matrix calculations
+// Matrix calculations
 void sum(int **m1, int **m2, int **result, int size);
 void subtract(int **m1, int **m2, int **result, int size);
 void multiply(int **m1, int **m2, int **result, int size);
