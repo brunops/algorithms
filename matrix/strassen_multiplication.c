b235c68ce8c6b2f462eba51ac09ca27356b8a05a
Bruno Sanches <brunopsanches@gmail.com>
2013-11-22 18:25:21 -0800

    Implement Strassen MAtrix Multiplication Algorithm
    * Implement Strassen algorithm following the coursera explanation and formulas
    * It seems that still it is not correctly multiplying
    * Add some debugging messages and formulas

--- matrix/strassen_multiplication.c
+++ matrix/strassen_multiplication.c
@@ -48,6 +48,9 @@ int main(int argc, char *argv[]) {
 
 void strassen(int **m1, int **m2, int **result, int size) {
   printf("STRASSEN!\n");
+  printf("\n\nmatrix 1\n");
+  print_matrix(m1, size);
+  printf("\n\nmatrix 2\n");
   print_matrix(m1, size);
 
   // Actual matrix size needed for the recursive calls
@@ -56,18 +59,37 @@ void strassen(int **m1, int **m2, int **result, int size) {
     resize_matrix(&m1, size, new_size);
     resize_matrix(&m2, size, new_size);
     resize_matrix(&result, size, new_size);
+    printf("\n\nresized matrix\n");
     print_matrix(m1, new_size);
   }
 
   strassen_recursive(m1, m2, result, new_size);
 
+  printf("\n\nresulting matrix\n");
   print_matrix(result, new_size);
 }
 
+/**
+ *
+ *
+ * Submatrices are defined as follows:
+ *  m1 = [
+ *    [A, B],
+ *    [C, D]
+ *  ]
+ *
+ *  m2 = [
+ *    [E, F],
+ *    [G, H]
+ *  ]
+ */
 void strassen_recursive(int **m1, int **m2, int **result, int size) {
   int i, j, new_size;
-  int **m1a, **m1b, **m1c, **m1d;
-  int **m2a, **m2b, **m2c, **m2d;
+  int **A, **B, **C, **D,
+      **E, **F, **G, **H,
+      **P1, **P2, **P3, **P4, **P5, **P6, **P7,
+      **R1, **R2, **R3, **R4,
+      **left, **right;
 
   // base case
   if (size == 1) {
@@ -76,37 +98,129 @@ void strassen_recursive(int **m1, int **m2, int **result, int size) {
   }
   else {
     new_size = size / 2;
-    allocate_matrix(&m1a, new_size);
-    allocate_matrix(&m1b, new_size);
-    allocate_matrix(&m1c, new_size);
-    allocate_matrix(&m1d, new_size);
-    allocate_matrix(&m2a, new_size);
-    allocate_matrix(&m2b, new_size);
-    allocate_matrix(&m2c, new_size);
-    allocate_matrix(&m2d, new_size);
+
+    // Allocate memory for all 8 sub matrices
+    // m1
+    allocate_matrix(&A, new_size);
+    allocate_matrix(&B, new_size);
+    allocate_matrix(&C, new_size);
+    allocate_matrix(&D, new_size);
+    // m2
+    allocate_matrix(&E, new_size);
+    allocate_matrix(&F, new_size);
+    allocate_matrix(&G, new_size);
+    allocate_matrix(&H, new_size);
 
     for (i = 0; i < new_size; i++) {
       for (j = 0; j < new_size; ++j) {
-        m1a[i][j] = m1[i][j];
-        m1b[i][j] = m1[i][j + new_size];
-        m1c[i][j] = m1[i + new_size][j];
-        m1d[i][j] = m1[i + new_size][j + new_size];
+        A[i][j] = m1[i][j];
+        B[i][j] = m1[i][j + new_size];
+        C[i][j] = m1[i + new_size][j];
+        D[i][j] = m1[i + new_size][j + new_size];
 
-        m2a[i][j] = m2[i][j];
-        m2b[i][j] = m2[i][j + new_size];
-        m2c[i][j] = m2[i + new_size][j];
-        m2d[i][j] = m2[i + new_size][j + new_size];
+        E[i][j] = m2[i][j];
+        F[i][j] = m2[i][j + new_size];
+        G[i][j] = m2[i + new_size][j];
+        H[i][j] = m2[i + new_size][j + new_size];
       }
     }
 
-    printf("M1 A");
-    print_matrix(m1a, new_size);
-    printf("M1 B");
-    print_matrix(m1b, new_size);
-    printf("M1 C");
-    print_matrix(m1c, new_size);
-    printf("M1 D");
-    print_matrix(m1d, new_size);
+    // Allocate memory for products (P's in Strassen's algorithm)
+    allocate_matrix(&P1, new_size);
+    allocate_matrix(&P2, new_size);
+    allocate_matrix(&P3, new_size);
+    allocate_matrix(&P4, new_size);
+    allocate_matrix(&P5, new_size);
+    allocate_matrix(&P6, new_size);
+    allocate_matrix(&P7, new_size);
+
+    // Allocate memory for left and right temp results
+    allocate_matrix(&left, new_size);
+    allocate_matrix(&right, new_size);
+
+    // Allocate memory for results
+    allocate_matrix(&R1, new_size);
+    allocate_matrix(&R2, new_size);
+    allocate_matrix(&R3, new_size);
+    allocate_matrix(&R4, new_size);
+
+    // Calculate products
+    // P1 = A (F - H)
+    subtract(F, H, right, new_size);
+    strassen_recursive(A, right, P1, new_size);
+
+    // P2 = (A + B) H
+    sum(A, B, left, new_size);
+    strassen_recursive(left, H, P2, new_size);
+
+    // P3 = (C + D) E
+    sum(C, D, left, new_size);
+    strassen_recursive(left, E, P3, new_size);
+
+    // P4 = D (G - E)
+    subtract(G, E, right, new_size);
+    strassen_recursive(D, right, P4, new_size);
+
+    // P5 = (A + D) (E + H)
+    sum(A, D, left, new_size);
+    sum(E, H, right, new_size);
+    strassen_recursive(left, right, P5, new_size);
+
+    // P6 = (B - D) (G + H)
+    subtract(B, D, left, new_size);
+    sum(G, H, right, new_size);
+    strassen_recursive(left, right, P6, new_size);
+
+    // P7 = (A - C) (E + F)
+    subtract(A, C, left, new_size);
+    sum(E, F, right, new_size);
+    strassen_recursive(left, right, P7, new_size);
+
+    // Calculate result, such that:
+    // m1 . m2 = [
+    //   [R1, R2],
+    //   [R3, R4]
+    // ]
+    // R1 = P5 + P4 - P2 + P6
+    sum(P5, P4, R1, new_size);
+    subtract(R1, P2, R1, new_size);
+    sum(R1, P6, R1, new_size);
+
+    // R2 = P1 + P2
+    sum(P1, P2, R2, new_size);
+
+    // R3 = P3 + P4
+    sum(P3, P4, R3, new_size);
+
+    // R4 = P1 + P5 - P3 - P7
+    sum(P1, P5, R4, new_size);
+    subtract(R4, P3, R4, new_size);
+    subtract(R4, P7, R4, new_size);
+
+    // Group everything into the result
+    for (i = 0; i < new_size; i++) {
+      for (j = 0; j < new_size; j++) {
+        result[i][j] = R1[i][j];
+        result[i][j + new_size] = R2[i][j];
+        result[i + new_size][j] = R3[i][j];
+        result[i + new_size][j + new_size] = R4[i][j];
+      }
+    }
+
+    // Deallocate memory of all P's and results
+    deallocate_matrix(P1, new_size);
+    deallocate_matrix(P2, new_size);
+    deallocate_matrix(P3, new_size);
+    deallocate_matrix(P4, new_size);
+    deallocate_matrix(P5, new_size);
+    deallocate_matrix(P6, new_size);
+    deallocate_matrix(P7, new_size);
+    deallocate_matrix(R1, new_size);
+    deallocate_matrix(R2, new_size);
+    deallocate_matrix(R3, new_size);
+    deallocate_matrix(R4, new_size);
+    deallocate_matrix(left, new_size);
+    deallocate_matrix(right, new_size);
   }
 }
 
