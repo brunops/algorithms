c116b1e2962974a04d481d2d724e06a6ad523ba8
Bruno Sanches <brunopsanches@gmail.com>
2013-11-27 17:13:36 -0800

    Implement first version of quicksort

--- sort/quicksort.rb
+++ sort/quicksort.rb
@@ -1,4 +1,54 @@
-def quicksort(ary, size)
+def quicksort(ary, low, high)
+  p "--------------------------- starting with: "
+  p "low: #{low}"
+  p "high: #{high}"
+  p ary
+  # Base case
+  return if high <= low
+
+  # Choose pivot
+  pivot_index = (high + low) / 2
+
+  p "PIVOT => #{ary[pivot_index]}"
+
+  # Take pivot out of the way
+  ary[pivot_index], ary[low] = ary[low], ary[pivot_index]
+  p ary
+  puts
+
+  # Start pointers
+  i = low + 1
+  j = low + 1
+
+  # Partitionate
+  while (j <= high)
+    puts
+    p "while with i: #{i} and j: #{j}"
+    if ary[j] < ary[low]
+      p "LESS! ary[#{j}]: #{ary[j]} ... pivot: #{ary[low]}"
+      p "SWAP! i: #{i} and j: #{j}"
+      # swap
+      ary[j], ary[i] = ary[i], ary[j]
+      i += 1
+    end
+    p ary
+
+    j += 1
+  end
+
+  # Set pivot in the correct place
+  new_pivot_index = i - 1
+  ary[low], ary[new_pivot_index] = ary[new_pivot_index], ary[low]
+
+
+  p "----- RESULT"
+  p ary
+  p "recursive call to partition 1: low: #{low}, high: #{new_pivot_index - 1}"
+  p "recursive call to partition 2: low: #{new_pivot_index + 1}, high: #{high}"
+
+  # Recurse through both partitions
+  quicksort(ary, low, new_pivot_index - 1)
+  quicksort(ary, new_pivot_index + 1, high)
 end
 
 def assert(obj)
@@ -6,5 +56,7 @@ def assert(obj)
 end
 
 if $0 == __FILE__
-  assert(quicksort([1, 3, 2], 3) == [1, 2, 3])
+  ary = [1, 3, 4, 5, 7, 0]
+  quicksort(ary, 0, 5)
+  assert(ary == [0, 1, 3, 4, 5, 7])
 end
