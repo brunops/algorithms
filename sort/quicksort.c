19f4eb4d96894ee68bf739dbea5e8ff25761eb40
Bruno Sanches <brunopsanches@gmail.com>
2013-11-27 18:16:18 -0800

    Partitionate array
    * Pivot element ends up with only smaller elements on its left and
    greater values on its right
    * Still missing recursive calls but it works for strict reversed sorted arrays

--- sort/quicksort.c
+++ sort/quicksort.c
@@ -28,7 +28,7 @@ int main() {
 }
 
 void quicksort(array ary, size_t low, size_t high) {
-  int i, j, pivot_index, temp;
+  int i, j, pivot, pivot_index, temp;
 
   print_array(ary);
 
@@ -39,11 +39,36 @@ void quicksort(array ary, size_t low, size_t high) {
 
   // Define median element as pivot
   pivot_index = low + high / 2;
+  pivot = ary.data[pivot_index];
 
   // Take pivot out of the way
   temp = ary.data[low];
   ary.data[low] = ary.data[pivot_index];
   ary.data[pivot_index] = temp;
+
+  // Initialize control indices for partition
+  i = low + 1;
+  j = low + 1;
+
+  // Partitionate
+  while (j <= high) {
+    if (ary.data[j] < pivot) {
+      // Swap
+      temp = ary.data[i];
+      ary.data[i] = ary.data[j];
+      ary.data[j] = temp;
+      i++;
+    }
+
+    j++;
+  }
+
+  // Put pivot back in its correct place
+  pivot_index = i - 1;
+  temp = ary.data[low];
+  ary.data[low] = ary.data[pivot_index];
+  ary.data[pivot_index] = temp;
+
 
   print_array(ary);
 }
