2d3af61d2124e58b857dd7d295e3d318e2fb043b
Bruno Sanches <brunopsanches@gmail.com>
2013-11-27 17:46:08 -0800

    Start quicksort C structure
    * Define array struct so there's no need to pass its size all over the place
    * Define print_array function for debugging
    * Define assert_array_sorted responsible to quit program if array is not sorted
    * Define empty quicksort function
    * Start with failing test

---
+++ sort/quicksort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct array {
+  int *data;
+  size_t size;
+} array;
+
+void assert_array_sorted(array ary);
+void print_array(array ary);
+void quicksort(array ary);
+
+int main() {
+  int i;
+
+  array a1;
+  a1.size = 5;
+
+  a1.data = malloc(a1.size * sizeof(int));
+  for (i = 0; i < a1.size; i++) {
+    a1.data[i] = a1.size - i;
+  }
+
+  quicksort(a1);
+  assert_array_sorted(a1);
+
+  return 0;
+}
+
+void quicksort(array ary) {
+
+}
+
+void assert_array_sorted(array ary) {
+  int i;
+
+  for (i = 0; i < ary.size - 1; ++i) {
+    if (ary.data[i] > ary.data[i + 1]) {
+      printf("FAIL! Array is NOT sorted!");
+      print_array(ary);
+      exit(1);
+    }
+  }
+}
+
+void print_array(array ary) {
+  int i;
+
+  printf("\nArray of size %ld: ", ary.size);
+  for (i = 0; i < ary.size; i++){
+    printf("%d ", ary.data[i]);
+  }
+}
