e7f6607470e24e9538c76f9ff42dc51e5290881c
Bruno Sanches <brunopsanches@gmail.com>
2014-01-13 06:26:48 -0800

    Describe algorithm with comments

--- misc/dp-total-coins.js
+++ misc/dp-total-coins.js
@@ -12,11 +12,24 @@
 function totalCoins(totalSum, coins) {
   // store perfect sum number of coins
   var coinsPerSum = {};
+
+  // Sum 0 is solved by 0 coins
   coinsPerSum[0] = 0;
 
+  // Solve subproblems for all sums until totalSum
   for (var sum = 1; sum <= totalSum; sum++) {
+    // Any amount will be less than Infinity
     coinsPerSum[sum] = Infinity;
+
+    // Loop through each of the given coins
     for (var coin = 0; coin < coins.length; coin++) {
+
+      // Update number of coins if c
+      // 1. Current coin is less than total sum
+      // and
+      // 2. Using the previous calculated subproblem (state sum - coins[coin])
+      // that will exactly solve this, and adding 1 (the current coin)
+      // gives a smaller number of coins than what was previously calculated
       if (coins[coin] <= sum && coinsPerSum[sum - coins[coin]] + 1 < coinsPerSum[sum]) {
         coinsPerSum[sum] = coinsPerSum[sum - coins[coin]] + 1;
       }
