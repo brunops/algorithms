3c0afec0e1a3d45b9ef60c215ee97cff18bacaa1
Bruno Sanches <brunopsanches@gmail.com>
2014-01-19 17:25:34 -0800

    Implement Stack#pop and return null for peek and pop when stack is empty

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -21,15 +21,28 @@ Stack.prototype.push = function(item) {
 };
 
 Stack.prototype.peek = function() {
-  return this.items[this.items.length - 1];
+  return this.items[this.items.length - 1] || null;
+};
+
+Stack.prototype.pop = function() {
+  return this.items.pop() || null;
 };
 
 // Tests
 var stack = new Stack();
+assert(stack.size() === 0, "stack starts empty");
+assert(stack.peek() === null, "#peek returns null when stack is empty");
 stack.push(5);
-assert(stack.size() === 1, "size of stack is 1 after pushing one item");
-assert(stack.peek() === 5, "peek() returns last pushed item");
-assert(stack.peek() === 5, "peek() does not remove the returned item");
+assert(stack.size() === 1, "#size of stack is 1 after pushing one item");
+assert(stack.peek() === 5, "#peek() returns last pushed item");
+assert(stack.peek() === 5, "#peek() does not remove the returned item");
+
+stack.push(50);
+assert(stack.size() === 2, "#push increases size of stack");
+assert(stack.pop() === 50, "#pop() returns last pushed item");
+assert(stack.size() === 1, "#pop() removes returned item");
+assert(stack.pop() === 5, "#pop() returns last pushed item");
+assert(stack.pop() === null, "#pop() returns null when stack is empty");
 
 console.log('success!');
 
