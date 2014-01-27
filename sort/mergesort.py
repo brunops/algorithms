ec2eff12951a3edf6b3e96fc2b113df7b24b1402
Bruno Sanches <brunopsanches@gmail.com>
2014-01-26 20:16:01 -0800

    Python mergesort

--- sort/mergesort.py
+++ sort/mergesort.py
@@ -1,4 +1,14 @@
-# def mergesort(ary):
+import math
+
+def mergesort(ary):
+  size = len(ary)
+  # already sorted
+  if size == 1:
+    return ary
+
+  middle = int(math.floor(size / 2))
+
+  return merge(mergesort(ary[0:middle]), mergesort(ary[middle:size]))
 
 
 # recursive merge for the lulz
@@ -18,3 +28,4 @@ def merge(a, b):
 
 
 print merge([1,2,30], [5, 6, 18])
+print mergesort([23,42,34,234,5,5,6,8,65,7,34,45,4,7,8,8,453,0])
