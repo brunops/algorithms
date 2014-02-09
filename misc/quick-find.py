4504fbb03ba7413ce048da1fc913501b2dbf6248
Bruno Sanches <brunopsanches@gmail.com>
2014-02-08 17:34:44 -0800

    Baby steps - Implement simplest #union method

--- misc/quick-find.py
+++ misc/quick-find.py
@@ -3,4 +3,21 @@
 # Use an Array as the data-structure to represent connectivity
 #   Two object p and q are connected if their entries in the Array are the same
 #
+class QuickFind:
+  def __init__(self, size):
+    # initialize structure without any connections between objects
+    self.ids = range(size)
 
+  # In this implementation, p is updated and q is left alone (doesn't matter the other)
+  def union(self, p, q):
+    self.ids[p] = self.ids[q] 
+
+
+# Tests
+qf = QuickFind(10)
+
+qf.union(4, 3)
+assert(qf.ids == [0, 1, 2, 3, 3, 5, 6, 7, 8, 9])
+
+
+print "success!"
