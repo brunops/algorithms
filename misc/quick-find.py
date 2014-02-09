7928be4e8c31598d278e49dd907e094e08ad9394
Bruno Sanches <brunopsanches@gmail.com>
2014-02-08 17:37:12 -0800

    Quick Find
    * #union => Update all ids in the same component as p
    * Add tests

--- misc/quick-find.py
+++ misc/quick-find.py
@@ -10,7 +10,10 @@ class QuickFind:
 
   # In this implementation, p is updated and q is left alone (doesn't matter the other)
   def union(self, p, q):
-    self.ids[p] = self.ids[q] 
+    pid = self.ids[p]
+    for i in range(len(self.ids)):
+      if self.ids[i] == pid:
+        self.ids[i] = self.ids[q] 
 
 
 # Tests
@@ -19,5 +22,7 @@ qf = QuickFind(10)
 qf.union(4, 3)
 assert(qf.ids == [0, 1, 2, 3, 3, 5, 6, 7, 8, 9])
 
+qf.union(3, 8)
+assert(qf.ids == [0, 1, 2, 8, 8, 5, 6, 7, 8, 9])
 
 print "success!"
