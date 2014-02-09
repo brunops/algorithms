8a69719bdf260716aca220b43fb61f2599c3760f
Bruno Sanches <brunopsanches@gmail.com>
2014-02-08 17:23:36 -0800

    Describe Quick Find connectivity problem

---
+++ misc/quick-find.py
@@ -0,0 +1,6 @@
+# Quick find
+# Dynamic Connectivity problem
+# Use an Array as the data-structure to represent connectivity
+#   Two object p and q are connected if their entries in the Array are the same
+#
+
