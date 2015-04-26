2ea0f3fbfadde0a41fe3e002c18ecb8523fd7616
Bruno Sanches <brunopsanches@gmail.com>
2015-04-25 18:14:33 -0700

    Define `lat?` which check if a given list is composed exclusively of atoms

--- misc/scheme-misc.scm
+++ misc/scheme-misc.scm
@@ -5,6 +5,19 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
+; check if given list is a list of atoms
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(lat? '(1 2 3 4 5)) ; -> #t
+(lat? '())          ; -> #t
+(lat? '(() 1))      ; -> #f
+(lat? '(1 2 3 ()))  ; -> #f
+
 ; check if `a` is a member of the list of atoms `lat`
 (define member?
   (lambda (a lat)
@@ -13,8 +26,8 @@
       ((eq? a (car lat)) #t)
       (else (member? a (cdr lat))))))
 
-(member? 1 '(1 2 3))
-(member? 2 '(1 2 3))
-(member? 3 '(1 2 3))
-(member? 1 '())
+;(member? 1 '(1 2 3))
+;(member? 2 '(1 2 3))
+;(member? 3 '(1 2 3))
+;(member? 1 '())
 
