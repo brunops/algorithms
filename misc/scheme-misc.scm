4b71d29b47e099908698feeb0a70cea0fe68d1d7
Bruno Sanches <brunopsanches@gmail.com>
2015-04-25 18:08:51 -0700

    Define `member?` function that checks if an atom is part os a list of atoms

--- misc/scheme-misc.scm
+++ misc/scheme-misc.scm
@@ -5,4 +5,16 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-(atom? 123)
+; check if `a` is a member of the list of atoms `lat`
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      ((eq? a (car lat)) #t)
+      (else (member? a (cdr lat))))))
+
+(member? 1 '(1 2 3))
+(member? 2 '(1 2 3))
+(member? 3 '(1 2 3))
+(member? 1 '())
+
