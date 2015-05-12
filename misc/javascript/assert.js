eaf34ea344b665ecd13909610b27b0c010f84dcd
Bruno Sanches <brunopsanches@gmail.com>
2015-05-12 00:34:06 -0700

    `assert` supports deepEqual (only for arrays)
    - It's a reaaaaly bad method, it assumes lot's of stuff but hey, it works :D

--- misc/javascript/assert.js
+++ misc/javascript/assert.js
@@ -1,10 +1,39 @@
 require('colors');
 
-module.exports = function (truth, msg) {
+function assert(truth, msg) {
   if (!truth) {
     throw new Error(('Fail: '.red) + (msg || '').yellow);
   }
   else {
     console.log(('Pass: '.green) + (msg || '').yellow);
   }
+};
+
+function deepEqual(actual, expected, msg) {
+  var isEqual = true;
+
+  if (actual.length !== expected.length) {
+    assert(false, msg);
+    return;
+  }
+
+  actual.forEach(function (el, i) {
+    isEqual &= el === expected[i];
+  });
+
+  if (!isEqual) {
+    msg += ('\nactual: ' + actual.toString()).bgRed.white.bold;
+    msg += ('\nexpected: ' + expected.toString()).bgGreen.white.bold;
+  }
+
+  assert(isEqual, msg);
+};
+
+module.exports = function () {
+  if (arguments.length === 3) {
+    deepEqual.apply(null, arguments);
+    return;
+  }
+
+  assert.apply(null, arguments);
 };
