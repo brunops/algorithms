ca485559967fbb7f1576e7fdf20cc1e9c227179e
Bruno Sanches <brunopsanches@gmail.com>
2015-05-12 00:02:31 -0700

    Use our favorite worst assert function for stack tests :D

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,9 +1,3 @@
-function assert(truthiness, msg) {
-  if (!truthiness) {
-    throw new Error("Failed: " + msg);
-  }
-}
-
 function Stack() {
   this.init();
 }
@@ -28,22 +22,26 @@ Stack.prototype.pop = function() {
   return this.items.pop() || null;
 };
 
-// Tests
-var stack = new Stack();
-assert(stack.size() === 0, "stack starts empty");
-assert(stack.peek() === null, "#peek returns null when stack is empty");
-stack.push(5);
-assert(stack.size() === 1, "#size of stack is 1 after pushing one item");
-assert(stack.peek() === 5, "#peek() returns last pushed item");
-assert(stack.peek() === 5, "#peek() does not remove the returned item");
+if (require.main === module) {
+  var assert = require('../misc/javascript/assert');
 
-stack.push(50);
-assert(stack.size() === 2, "#push increases size of stack");
-assert(stack.pop() === 50, "#pop() returns last pushed item");
-assert(stack.size() === 1, "#pop() removes returned item");
-assert(stack.pop() === 5, "#pop() returns last pushed item");
-assert(stack.pop() === null, "#pop() returns null when stack is empty");
+  // Tests
+  var stack = new Stack();
+  assert(stack.size() === 0, "stack starts empty");
+  assert(stack.peek() === null, "#peek returns null when stack is empty");
+  stack.push(5);
+  assert(stack.size() === 1, "#size of stack is 1 after pushing one item");
+  assert(stack.peek() === 5, "#peek() returns last pushed item");
+  assert(stack.peek() === 5, "#peek() does not remove the returned item");
 
-console.log('success!');
+  stack.push(50);
+  assert(stack.size() === 2, "#push increases size of stack");
+  assert(stack.pop() === 50, "#pop() returns last pushed item");
+  assert(stack.size() === 1, "#pop() removes returned item");
+  assert(stack.pop() === 5, "#pop() returns last pushed item");
+  assert(stack.pop() === null, "#pop() returns null when stack is empty");
+
+  console.log('success!');
+}
 
 
