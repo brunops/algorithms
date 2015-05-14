b381bd499443f831894a269c9b84830526537ee3
Bruno Sanches <brunopsanches@gmail.com>
2015-05-13 23:44:16 -0700

    Define List#forEach and make `linked-linst2.js` requirable

--- data-structures/linked-list2.js
+++ data-structures/linked-list2.js
@@ -135,6 +135,18 @@ LinkedList.prototype = {
     return size;
   },
 
+  forEach: function (callback) {
+    var curr = this.head;
+
+    while (curr) {
+      if (callback) {
+        callback(curr.data, curr);
+      }
+
+      curr = curr.next;
+    }
+  },
+
   print: function () {
     var curr = this.head;
 
@@ -150,77 +162,89 @@ module.exports = {
   LinkedList: LinkedList
 };
 
-var list = new LinkedList();
 
-list.insertEnd(5);
-list.insertEnd(5);
-list.insertEnd(15);
+if (require.main === module) {
+  var assert = require('../misc/javascript/assert');
+  var list = new LinkedList();
 
-
-list.print();
-console.log('-------- 30 after 15');
-list.insertAfter(30, 15);
-list.print();
-
-console.log('-------- 1 after 5');
-list.insertAfter(1, 5);
-list.print();
-
-console.log('-------- 7 after 15');
-list.insertAfter(7, 15);
-list.print();
+  list.insertEnd(5);
+  list.insertEnd(5);
+  list.insertEnd(15);
 
 
-console.log('-------- 70 after 15000 (non existing number)');
-list.insertAfter(70, 15000);
-list.print();
+  list.print();
+  console.log('-------- 30 after 15');
+  list.insertAfter(30, 15);
+  list.print();
 
-console.log('-------- 1 before 15000 (non existing number)');
-list.insertBefore(1, 15000);
-list.print();
+  console.log('-------- 1 after 5');
+  list.insertAfter(1, 5);
+  list.print();
 
-console.log('-------- 1 before 5');
-list.insertBefore(1, 5);
-list.print();
-
-console.log('-------- 11 before 30');
-list.insertBefore(11, 30);
-list.print();
-
-console.log('-------- 55 before 15');
-list.insertBefore(55, 15);
-list.print();
+  console.log('-------- 7 after 15');
+  list.insertAfter(7, 15);
+  list.print();
 
 
+  console.log('-------- 70 after 15000 (non existing number)');
+  list.insertAfter(70, 15000);
+  list.print();
 
-console.log('-------- [Cycle List]');
+  console.log('-------- 1 before 15000 (non existing number)');
+  list.insertBefore(1, 15000);
+  list.print();
 
-console.log('First list with no cycles, cycle starts at: %s', list.findLoopBeginning());
-console.log('Loop size: %s', list.getLoopSize());
+  console.log('-------- 1 before 5');
+  list.insertBefore(1, 5);
+  list.print();
+
+  console.log('-------- 11 before 30');
+  list.insertBefore(11, 30);
+  list.print();
+
+  console.log('-------- 55 before 15');
+  list.insertBefore(55, 15);
+  list.print();
+
+  var result = [];
+  list.forEach(function (data) {
+    result.push(data);
+  });
+
+  assert(result, [1, 5, 1, 5, 55, 15, 7, 11, 30], '#forEach visits each node');
 
 
-var list2 = new LinkedList();
-console.log('\nnew list:')
-console.log(' 1-2-3-5-8');
-console.log('        / \\');
-console.log('      35   13');
-console.log('       |    \\');
-console.log('       30    15');
-console.log('        \\   /');
-console.log('        25-21');
 
-list2.insertEnd(1);
-list2.insertEnd(2);
-list2.insertEnd(3);
-list2.insertEnd(5);
-var cycleStart = list2.insertEnd(8);
-list2.insertEnd(13);
-list2.insertEnd(15);
-list2.insertEnd(21);
-list2.insertEnd(25);
-list2.insertEnd(30);
-var tail = list2.insertEnd(35);
-tail.next = cycleStart;
+  console.log('-------- [Cycle List]');
 
-console.log('Cycle starts at node: %s', list2.findLoopBeginning().data);
-console.log('Loop size: %s', list2.getLoopSize());
+  console.log('First list with no cycles, cycle starts at: %s', list.findLoopBeginning());
+  console.log('Loop size: %s', list.getLoopSize());
+
+
+  var list2 = new LinkedList();
+  console.log('\nnew list:')
+  console.log(' 1-2-3-5-8');
+  console.log('        / \\');
+  console.log('      35   13');
+  console.log('       |    \\');
+  console.log('       30    15');
+  console.log('        \\   /');
+  console.log('        25-21');
+
+  list2.insertEnd(1);
+  list2.insertEnd(2);
+  list2.insertEnd(3);
+  list2.insertEnd(5);
+  var cycleStart = list2.insertEnd(8);
+  list2.insertEnd(13);
+  list2.insertEnd(15);
+  list2.insertEnd(21);
+  list2.insertEnd(25);
+  list2.insertEnd(30);
+  var tail = list2.insertEnd(35);
+  tail.next = cycleStart;
+
+  console.log('Cycle starts at node: %s', list2.findLoopBeginning().data);
+  console.log('Loop size: %s', list2.getLoopSize());
+
+}
